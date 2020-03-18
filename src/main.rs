74157495f186408adf28168baa7d584019f23701
dark.H <dark.H>
2020-03-18 22:49:44 +0800

    check it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 extern crate minreq;
 use std::result::Result;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::str;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+type Out<T> =  Result<T, Box<dyn std::error::Error>>;
+
+fn download(url :&str) -> Out<Vec<u8>>{
+    
+    let response = minreq::get(url).send()?;
+    Ok(response.into_bytes())
+}
+
+
+fn main() -> Out<()> {
+    if let Ok(tmp_bytes) =  download("https://115.236.8.152:50443/dr/kcpee-min/-/raw/master/index.list"){
+        let index = match str::from_utf8(&tmp_bytes){
+            Ok(s) => s.split('\n').collect(),
+            Err(_) => vec![]
+        };
+        for i in index{
+            println!("Down {}", i)
+        }
+    }else{
+        println!("Err : with index" )
+    };
+    
     
     // sync post request of some json.
-    let response = minreq::get("http://httpbin.org/ip").send()?;
-    println!("{}",response.as_str()?);
     Ok(())
 }
