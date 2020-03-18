db6d7c51a19f31d8e2ac33fbfc3958197ee62a46
dark.H <dark.H>
2020-03-19 01:27:28 +0800

    check it

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,65 @@
-extern crate minreq;
+
 use std::result::Result;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use std::io::Write;
 use std::str;
+use std::process::{Command, Stdio};
+use std::fs::read_to_string;
 
 
 type Out<T> =  Result<T, Box<dyn std::error::Error>>;
 
-fn download(url :&str) -> Out<Vec<u8>>{
+
+
+fn download<F>(url :&str, dst :&str,after : Option<F> )-> Out<()>
+where F: FnOnce(&str)  +Send
+{
     
-    let response = minreq::get(url).send()?;
-    Ok(response.into_bytes())
-}
-
-
-fn main() -> Out<()> {
-    if let Ok(tmp_bytes) =  download("https://115.236.8.152:50443/dr/kcpee-min/-/raw/master/index.list"){
-        let index = match str::from_utf8(&tmp_bytes){
-            Ok(s) => s.split('\n').collect(),
-            Err(_) => vec![]
-        };
-        for i in index{
-            println!("Down {}", i)
-        }
+    let mut dst_uri  = format!("C:\\tmp\\{}" , dst); 
+    let _ = if cfg!(target_os = "windows") {
+        
+        let wincmd = format!("client = new-object System.Net.WebClient;client.DownloadFile('{}','{}');",url, dst_uri );
+        let mut process = Command::new("powershell")
+                      .args(&["-Command", "-"])
+                      .stdin(Stdio::piped())
+                      .spawn()?;
+        let stdin = process.stdin.as_mut().expect("pipe failure");
+        stdin.write_all(wincmd.as_bytes()).expect("ps downlaod error");
+    } else {
+        dst_uri = format!("/tmp/{}", dst);
+        let cmd = format!("wget -c '{}' -O '/tmp/{}');",url, dst_uri );
+        Command::new("bash")
+                .arg("-c")
+                .arg(cmd)
+                .output().expect("may wget error ");
+    };
+    if let Some(after_fun) = after{
+        after_fun(&dst_uri);
+        Ok(())
     }else{
-        println!("Err : with index" )
+        Ok(())
+    }
+}
+fn read_index (filepath: &str) {
+    let sep = if cfg!(target_os="windows"){
+        "\r\n"
+    }else{
+        "\n"
     };
     
+    let _ = match read_to_string(filepath){
+        Ok(ss) => {
+            let _ = ss.split(sep).collect::<Vec<&str>>().iter().map(|&each_name| {
+                println!("found new file to donwload: {}", each_name)
+            }).collect::<Vec<_>>();
+        },
+        Err(e ) => println!("{}",e)
+    };
     
+}
+
+fn main() -> Out<()> {
+   
+    download("https://115.236.8.152:50443/dr/kcpee-min/-/raw/master/index.list", "index.list", Some(read_index) )
     // sync post request of some json.
-    Ok(())
+    // Ok(())
 }
